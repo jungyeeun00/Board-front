002fb9da1fba95a38a9bb8f01dba694e0f6a2ac1
LeeGaIn00 <rkdls7815@naver.com>
2022-03-09 02:04:33 +0900

    ga

--- paper-kit-react-main/src/components/Headers/IndexHeader.js
+++ paper-kit-react-main/src/components/Headers/IndexHeader.js
@@ -53,7 +53,7 @@ function IndexHeader() {
               </div>
             </div>
             <h2 className="presentation-subtitle text-center">
-              Be Vegin ;;
+              Be Vegin ;
             </h2>
           </Container>
         </div>
